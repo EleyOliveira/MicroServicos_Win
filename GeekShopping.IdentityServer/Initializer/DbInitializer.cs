430a66118258cecef1cae4d1f4a775a14853a3fe
Eleyfeson <eleyoliveira@bol.com.br>
2023-03-28 16:10:06 -0300

    Configuração do banco do Identity Server

---
+++ GeekShopping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+
+namespace GeekShopping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly MySQLContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<ApplicationUser> _role;
+
+        public DbInitializer(MySQLContext context, UserManager<ApplicationUser> user, RoleManager<ApplicationUser> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
