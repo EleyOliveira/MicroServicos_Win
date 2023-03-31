956ae47dced13f4b5733ace7256bf6d0da20308b
Eleyfeson <eleyoliveira@bol.com.br>
2023-03-31 16:23:44 -0300

    implementando autenticação web.

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -10,6 +10,20 @@ builder.Services.AddHttpClient<IProductService,ProductService>(
     c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductApi"])
     );
 
+builder.Services.AddAuthentication(options => 
+{ 
+    options.DefaultScheme = "Cookie";
+    options.DefaultChallengeScheme = "oidc";
+
+}).AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+  .AddOpenIdConnect("oidc", options => 
+  {
+      options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+      options.GetClaimsFromUserInfoEndpoint = true;
+      options.ClientId = "geek_shopping";
+      options.ClientSecret = "my_super_secret";
+  });
+
 //Permite que a propriedade Price reconheça o ponto decimal
 var cultureInfo = new CultureInfo("pt-BR");
 cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
