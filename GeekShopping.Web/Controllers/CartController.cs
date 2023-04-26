10f73d671a614a501c0c4721662e0ff97b772e28
Eleyfeson <eleyoliveira@bol.com.br>
2023-04-26 20:17:44 -0300

    Tela de carrinho

--- GeekShopping.Web/Controllers/CartController.cs
+++ GeekShopping.Web/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using GeekShopping.Web.Models;
 using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers
@@ -16,6 +17,7 @@ namespace GeekShopping.Web.Controllers
             _cartService = cartService ?? throw new ArgumentNullException(nameof(cartService));
         }
 
+        [Authorize]
         public async Task<IActionResult> CartIndex()
         {
             return View(await FindUserCart());
