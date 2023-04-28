f3d9403349fc13487db7ef0dc625d9269441ade3
Eleyfeson <eleyoliveira@bol.com.br>
2023-04-28 15:42:52 -0300

    Serviço coupon

---
+++ GeekShopping.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly MySQLContext _context;
+        private IMapper _mapper;
+
+        public CouponRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<CouponVO> GetCouponByCouponCode(string couponCode)
+        {
+            var coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.CouponCode == couponCode);
+            return _mapper.Map<CouponVO>(coupon);
+        }
+    }
+}
+
